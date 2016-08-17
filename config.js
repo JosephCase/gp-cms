ed957d2c3863ff5f86dbce8538cd59ce402f18d4
Joseph Case <joseph.case@hotmail.co.uk>
2016-08-17 01:38:20 +0100

    moved config to json file

--- config.js
+++ config.js
@@ -1,11 +1,4 @@
-exports.contentDirectory = 'content/';
-exports.imageSizes = [1400, 1000, 700, 500];
-exports.videoFormats = [{ext: 'mp4', codec: 'libx264'}, {ext: 'webm', codec: 'libvpx'}];
-exports.databaseLogin = {
-	host: '50.62.209.149',
-	port: '3306',
-	user: 'JosephCase',
-	password: 'Ls962_aj',
-	database: 'giusy_test',
-	multipleStatements: true
-}
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+module.exports = config;
